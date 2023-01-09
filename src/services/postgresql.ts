88383c5405c2ebb8de38f19d31d2117428b8fd17
21Chani <dmendoncacosta03@gmail.com>
2023-01-09 15:00:25 -0300

    debug

--- src/services/postgresql.ts
+++ src/services/postgresql.ts
@@ -5,14 +5,25 @@ export const postgres = new Sequelize(PSQL_DATABASE, PSQL_USER, PSQL_PASSWORD, {
   dialect: 'postgres',
   host: PSQL_HOST,
   port: PSQL_PORT,
+  ssl: true,
 })
 
 export async function connectDb() {
   const date = Date.now()
-  import('../models/groups/group.postgres')
-  await postgres.sync({ logging: false })
-  const ms = Date.now() - date
-  console.log(`Database connected successfuly. ${ms}ms`)
+  console.log('\n---------------------------')
+  console.log('Connecting to postgres instance...')
+  try {
+    import('../models/groups/group.postgres')
+    await postgres.sync({ logging: false })
+    const ms = Date.now() - date
+
+    // Log
+    console.log(`Database connected successfuly. ${ms}ms`)
+    console.log('---------------------------\n')
+  } catch (e) {
+    console.log('Ocurred an error connecting to db. ')
+    if (e instanceof Error) console.log('error:', e.message)
+  }
 }
 
 export async function disconnectDb() {
