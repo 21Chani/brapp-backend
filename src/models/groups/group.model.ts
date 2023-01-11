7f600039bcce9a4a159fb249a3713535d2cad70e
21Chani <dmendoncacosta03@gmail.com>
2023-01-11 09:26:19 -0300

    move db connection to model functions

--- src/models/groups/group.model.ts
+++ src/models/groups/group.model.ts
@@ -1,16 +1,21 @@
 import { v4 as uuidV4 } from 'uuid'
+import { connectMongo, disconnectMongo } from '../../services/mongo'
 import { Group, Groups } from './group.mongo'
 
 export async function hasGroup(uuid: string) {
+  await connectMongo()
   const group = await Groups.findOne({ uuid })
   const hasGroup = Boolean(group)
+  await disconnectMongo()
   return hasGroup
 }
 
 export async function createGroup(data: Group) {
   try {
+    await connectMongo()
     const uuid = uuidV4()
     const group = await Groups.create({ ...data, uuid })
+    await disconnectMongo()
     return { group, error: undefined }
   } catch (error) {
     if (!(error instanceof Error)) return { error, group: undefined }
--- src/routes/group/group.controller.ts
+++ src/routes/group/group.controller.ts
@@ -5,16 +5,12 @@ import { connectMongo, disconnectMongo } from '../../services/mongo'
 
 export async function httpCreateGroup(req: Request<any, any, Group>, res: Response) {
   const data = req.body
-  await connectMongo()
   const { error, group } = await createGroup(data)
   if (error) return res.status(500).json({ error })
-  await disconnectMongo()
   return res.json({ status: 'ok', group: data })
 }
 
 export async function httpGetAllGroups(req: Request, res: Response) {
-  await connectMongo()
   const groups = await Groups.find({}, { __id: 0, __v: 0 })
-  await disconnectMongo()
   return res.json({ ...groups })
 }
